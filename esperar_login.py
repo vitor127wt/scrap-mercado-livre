59bada3bd10828236e54cd62015efec6314e54dc
Vitor W.R Teixeira <vitor127wt@gmail.com>
2026-05-27 19:13:08 -0300

    Correção de texto

--- esperar_login.py
+++ esperar_login.py
@@ -3,7 +3,7 @@ import sys
 
 def esperar_login():
     print('É necessário realizar o login no site do Mercado Livre para proseguir')
-    resposta = dependency_checker.perguntar(f'Após realizar o login digite [Y] para continuar, caso não queira logar digite [N] para fechar o programa \n')
+    resposta = dependency_checker.perguntar(f'Após realizar o login digite [Y] para continuar, caso não queira logar digite [N] para fechar o programa \n ->')
     if not resposta:
         sys.exit(0)
     else:
