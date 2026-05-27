3429601442879a3f170a4c1db1e7b52eecccff61
Vitor W.R Teixeira <vitor127wt@gmail.com>
2026-05-27 16:03:04 -0300

    Criada Verificação de Dependencia

---
+++ dependency_checker.py
@@ -0,0 +1,44 @@
+import sys
+import subprocess
+import time
+
+def verificar_dependencias():
+    DEPENDENCIAS = ['selenium', 'webdriver-manager', 'pandas']
+    for dependencia in DEPENDENCIAS:
+        nome_pacote = dependencia.replace('-', '_')
+        try:
+            __import__(nome_pacote)
+        except ModuleNotFoundError:
+            print(f'Pacote: {nome_pacote}, não encontrado. Deseja instalar ?')
+            if perguntar():
+                instalar_pacote(nome_pacote)
+            else:
+                print('Encerrando processo')
+                time.sleep(1)
+                sys.exit(0)
+            
+
+
+def instalar_pacote(pacote):
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', pacote])
+        print(f'Pacote {pacote}, instalado com sucesso')
+    except Exception as e:
+        print(f'Erro ao instalar {pacote}: {e}')
+        time.sleep(1)
+        sys.exit(1)
+
+def perguntar() -> bool:
+    resposta = input('[Y] Para instalar. [N] Para sair.')
+    resposta.upper()
+    if not validar_resposta(resposta):
+        print('Entrada invalida.')
+        perguntar()
+    return True if resposta == 'Y' else False
+
+def validar_resposta(resposta) -> bool:
+    if len(resposta) != 1:
+        return False
+    if resposta not in 'YN':
+        return False
+    return True
